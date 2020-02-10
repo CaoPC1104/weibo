a5c40b35ce041826baf97737521ae5defad145f5
PC Cao <823702933@qq.com>
2020-02-10 21:52:03 +0800

    Initial commit

---
+++ resources/views/help.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html >
+<head>
+
+    <title>Weibo App</title>
+</head>
+<body>
+    <h1>帮助页</h1>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+    <title>Weibo App</title>
+</head>
+<body>
+    <h1>主页</h1>
+</body>
+</html>
